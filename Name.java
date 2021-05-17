21214019feaa9152c342abb7667fac14497ac257
Sara-Hamed <83507241+Sara-Hamed@users.noreply.github.com>
2021-05-17 23:30:13 +0200

    Add files via upload

---
+++ Name.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Name {
+
+    public static void main(String[] args) {
+        String name = new String();
+        System.out.println("Enter your name: ");
+        Scanner input = new Scanner(System.in);
+        name = input.nextLine();
+
+        System.out.println("You entered : " + name);
+        String temp = new String(name.toUpperCase());
+
+        System.out.println(temp);
+
+        char c = temp.charAt(0);
+        System.out.print(c);
+
+        for (int i = 1; i < temp.length(); i++) {
+            char c1 = temp.charAt(i);
+
+            if (c1 == ' ') {
+                System.out.print(temp.charAt(i + 1));
+            }
+        }
+    }
+}
+
